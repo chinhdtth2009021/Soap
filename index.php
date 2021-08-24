f6cd143bef19d1b3afd35a0e7400d39be4a6df35
Chinh DT <chinhdtth2009021@fpt.edu.vn>
2021-08-24 10:50:04 +0700

    Soap

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$soap = new SoapClient('http://www.learnwebservices.com/services/hello?WSDL');
+$response = $soap->SayHello(array(
+    'HelloRequest' => array(
+        'Name' => $_GET['name']
+    )
+));
+echo $response->HelloResponse->Message;
